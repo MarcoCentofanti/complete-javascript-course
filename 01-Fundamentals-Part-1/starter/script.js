663aa2d7780866689d8b4894b42165c97c2486ca
marcocentofanti <marcocentofanti@hotmail.it>
2025-10-20 16:40:21 +0200

    up to Lesson 20

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -10,7 +10,7 @@ console.log(60 + 30);
 console.log(60 + 30);
 
 */
-
+/*
 console.log(60 + 30);
 const firstName = "Marco";
 const job = "Soundengineer";
@@ -36,3 +36,41 @@ lines"
 console.log(`String with
 multiples
 lines`);
+*/
+/*
+const age = 16;
+const isOldEnough = age >= 18;
+
+if (isOldEnough) {
+  console.log("Sarah can start driving license");
+} else {
+  const yearsLeft = 18 - age;
+  if (yearsLeft > 1) {
+    console.log(`Sarah cannot start driving license. wait ${yearsLeft} years`);
+  } else {
+    console.log(`Sarah cannot start driving license. wait ${yearsLeft} year`);
+  }
+}
+*/
+
+// LESSON 20
+
+// TYPE CONVERSION
+const inputYear = "1991";
+console.log(Number(inputYear), inputYear);
+console.log(Number(inputYear) + 18);
+
+console.log(Number("Jonas"));
+console.log(typeof NaN);
+
+console.log(String(23), 23);
+
+// TYPE COERCION
+console.log("i am" + 23 + "uears old");
+console.log("23" - "10" - 3);
+console.log("23" + "10" + 3);
+console.log("23" / 2);
+
+let n = "1" + 1;
+n = n - 1;
+console.log(n);
