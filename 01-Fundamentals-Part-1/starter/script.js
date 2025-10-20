df3c2cd7bbc1bfa5d668466735a994a84d6a9701
marcocentofanti <marcocentofanti@hotmail.it>
2025-10-20 14:09:16 +0200

    Start to 17 lesson

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,1 +1,38 @@
-console.log(60+30)
+/*
+
+console.log(60 + 30);
+console.log(60 + 30);
+console.log(60 + 30);
+console.log(60 + 30);
+console.log(60 + 30);
+console.log(60 + 30);
+console.log(60 + 30);
+console.log(60 + 30);
+
+*/
+
+console.log(60 + 30);
+const firstName = "Marco";
+const job = "Soundengineer";
+const birthYear = 1987;
+const year = 2025;
+
+const Marco =
+  "I'm " + firstName + ", a " + (year - birthYear) + " years old " + job + "!";
+
+console.log(Marco);
+
+const marcoNew = `I'm ${firstName}, a ${year - birthYear} years old ${job}!`;
+console.log(marcoNew);
+
+console.log(`Just a regular string...`);
+
+console.log(
+  "String with \n\
+multiples\n\
+lines"
+);
+
+console.log(`String with
+multiples
+lines`);
